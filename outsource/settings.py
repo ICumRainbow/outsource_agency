8f493e81a63bc5c8bcb6592e4e1f1d6aeb86eaff
Ernest <onetwo20003@gmail.com>
2023-02-07 16:18:14 +0500

    sensitive data hidden in .env

--- outsource/settings.py
+++ outsource/settings.py
@@ -13,6 +13,11 @@ import os
 from os.path import join
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d=k1np+y()fj(6qd+6lb&sd=0%798^am$q925@*rckd+r9ir=v'
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
